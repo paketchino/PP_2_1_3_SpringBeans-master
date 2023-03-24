703bbeea7974e231e8a5d2f9cca4cec8ce32cd61
paketchino <chansgorn@gmail.com>
2023-03-25 00:18:30 +0300

    Initial commit

---
+++ src/main/java/app/Application.java
@@ -0,0 +1,26 @@
+package app;
+
+import app.config.AppConfig;
+import app.model.AnimalsCage;
+import app.model.Timer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.Time;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        for (int i = 0; i < 5; i++) {
+            AnimalsCage bean =
+                    applicationContext.getBean(AnimalsCage.class);
+            bean.whatAnimalSay();
+        }
+        Timer bean = applicationContext.getBean(Timer.class);
+        System.out.println(bean.getTime());
+
+    }
+
+}
---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,28 @@
+package app.config;
+
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+
+    @Bean("Dog")
+    public Dog getDog() {
+        Dog dog = new Dog();
+        System.out.println(dog.toString());
+        return dog;
+    }
+
+//    @Bean("Timer")
+//    public Timer getTimer() {
+//        Timer timer = new Timer();
+//        System.out.println(timer.getTime());
+//        return timer;
+//    }
+}
